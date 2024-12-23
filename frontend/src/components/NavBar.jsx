e3fb5ca91060089f649e419eadb18aed70949104
ashimsharma <sharmaashim756@gmail.com>
2024-12-23 15:43:46 -0500

    Update NavBar Code - Design Complete

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import NavBarIcon from "../images/NavBar-Icon.png";
+import UserIcon from "../images/User-Icon.png";
 
-export default function NavBar(){
+export default function NavBar() {
     return (
         <>
-            <nav className="bg-slate-900 text-white p-4 border-b-2">
+            <nav className="bg-slate-800 text-white p-2 border-b-2 flex">
                 <span>
-                    <img src="../assets/NavBar-Icon.png" alt="Icon" className="display-inline"/>
-                    <span>
-                        <h1>Drum Kit</h1>
-                    </span>
+                    <img src={NavBarIcon} alt="Icon" className="h-16" />
                 </span>
-                <span></span>
+
+                <span className="flex items-center text-2xl">
+                    <h1>Drum Kit</h1>
+                </span>
+
+                <span className="basis-9/12 flex justify-center">
+                    <ul className="flex items-center text-lg gap-6">
+                        <li className="hover:text-slate-400 hover:cursor-pointer">Home</li>
+                        <li className="hover:text-slate-400 hover:cursor-pointer">Recorded Sounds</li>
+                    </ul>
+                </span>
+
+                <span className="flex-grow flex justify-end">
+                    <img src={UserIcon} alt="User" className="h-14" />
+                </span>
             </nav>
         </>
     );
