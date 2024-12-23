57ef3e258fddb887c79887cffcbdbfd68dfc0a06
ashimsharma <sharmaashim756@gmail.com>
2024-12-23 14:51:23 -0500

    Add NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-
+import NavBar from "./components/NavBar.jsx"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='text-2xl text-blue-600 bg-slate-900 p-20 m-0 text-center'>DRUMKIT - COMING SOON</div>
+      <NavBar/>
     </>
   )
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,1 +1,17 @@
 import React from "react";
+
+export default function NavBar(){
+    return (
+        <>
+            <nav className="bg-slate-900 text-white p-4 border-b-2">
+                <span>
+                    <img src="../assets/NavBar-Icon.png" alt="Icon" className="display-inline"/>
+                    <span>
+                        <h1>Drum Kit</h1>
+                    </span>
+                </span>
+                <span></span>
+            </nav>
+        </>
+    );
+}
