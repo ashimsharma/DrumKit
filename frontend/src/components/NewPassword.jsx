3b403842372af126247d1d6130b5ac4651ea8ec6
ashimsharma <sharmaashim756@gmail.com>
2025-02-07 19:15:03 -0500

    New Password after forgot password frontend complete.

--- frontend/src/components/NewPassword.jsx
+++ frontend/src/components/NewPassword.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useWatch } from "react-hook-form";
 import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
-import { IoChevronBackCircleSharp, IoEye, IoEyeOff } from "react-icons/io5"; 
-import Loader from "./Loader.jsx"; 
+import { IoChevronBackCircleSharp, IoEye, IoEyeOff } from "react-icons/io5";
+import Loader from "./Loader.jsx";
 import PopUp from "./PopUp.jsx";
 
 export default function NewPassword() {
     const prevLocation = useLocation().state?.from;
 
     useEffect(() => {
-        if(!prevLocation){
+        if (!prevLocation) {
             navigate("/login");
             return;
         }
@@ -20,16 +20,19 @@ export default function NewPassword() {
     const [loading, setLoading] = useState(true);
     const [showNotification, setShowNotification] = useState({ show: false, positiveMessage: true, message: '' });
 
-    const [showOldPassword, setShowOldPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
     const [showNewPassword, setShowNewPassword] = useState(false);
 
     const {
         register,
         handleSubmit,
+        watch,
         formState: { errors },
     } = useForm({
         mode: "onChange",
     });
+
+    const newPassword = watch('newPassword');
 
     const navigate = useNavigate();
 
@@ -38,35 +41,7 @@ export default function NewPassword() {
     };
 
     const onSubmit = async (data) => {
-        try {
-            setShowNotification({ show: true, positiveMessage: true, message: 'Updating Password...' });
-            const response = await axios.patch(
-                `${import.meta.env.VITE_API_URL}/users/update-password`,
-                data,
-                {
-                    withCredentials: true,
-                }
-            );
-
-            if (response) {
-                setShowNotification({ show: true, positiveMessage: true, message: response.data.message });
-            }
-
-            setTimeout(() => {
-                setShowNotification({ show: false, positiveMessage: false, message: '' });
-                navigate('/profile');
-            }, 1500);
-        } catch (error) {
-            if (!error.response?.data) {
-                navigate("/login");
-                return;
-            }
-
-            setShowNotification({ show: true, positiveMessage: false, message: error.response?.data.message });
-            setTimeout(() => {
-                setShowNotification({ show: false, positiveMessage: false, message: '' });
-            }, 1500);
-        }
+        console.log(data);
     };
 
     if (loading) {
@@ -91,44 +66,41 @@ export default function NewPassword() {
                     onSubmit={handleSubmit(onSubmit)}
                     className="w-full max-w-md p-8 bg-gray-800 rounded-lg shadow-lg"
                 >
-                    <h2 className="text-3xl font-bold mb-6 text-center">Update Password</h2>
+                    <h2 className="text-3xl font-bold mb-6 text-center">Generate New Password</h2>
 
                     {/* Old Password Field */}
                     <div className="relative">
                         <input
-                            type={showOldPassword ? "text" : "password"}
-                            placeholder="Enter Your Old Password"
+                            type={showNewPassword ? "text" : "password"}
+                            placeholder="Enter Password"
                             className="bg-transparent border border-slate-400/50 rounded-lg px-4 py-2 w-full text-white placeholder:text-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-300 focus:border-slate-500 backdrop-blur-sm my-4"
-                            {...register("oldPassword", { required: "All fields are required.", minLength: { value: 8, message: "Password must be 8 characters long." }, maxLength: { value: 15, message: "Too long Password." } })}
+                            {...register("newPassword", { required: "All fields are required.", minLength: { value: 8, message: "Password must be 8 characters long." }, maxLength: { value: 15, message: "Too long Password." } })}
                         />
                         <div
                             className="absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-200"
-                            onClick={() => setShowOldPassword(!showOldPassword)}
+                            onClick={() => setShowNewPassword(!showNewPassword)}
                         >
-                            {showOldPassword ? <IoEyeOff size={20} /> : <IoEye size={20} />}
+                            {showNewPassword ? <IoEyeOff size={20} /> : <IoEye size={20} />}
                         </div>
-                        {errors.oldPassword && (
-                            <p className="text-red-500 text-sm mt-1">{errors.oldPassword.message}</p>
-                        )}
                     </div>
 
                     {/* New Password Field */}
                     <div className="relative">
                         <input
-                            type={showNewPassword ? "text" : "password"}
-                            placeholder="Enter Your New Password"
+                            type={showConfirmPassword ? "text" : "password"}
+                            placeholder="Confirm Password"
                             className="bg-transparent border border-slate-400/50 rounded-lg px-4 py-2 w-full text-white placeholder:text-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-300 focus:border-slate-500 backdrop-blur-sm my-4"
-                            {...register("newPassword", { required: "All fields are required.", minLength: { value: 8, message: "Password must be 8 characters long." }, maxLength: { value: 15, message: "Too long Password." } })}
+                            {...register("confirmPassword", {
+                                required: "All fields are required.", minLength: { value: 8, message: "Password must be 8 characters long." }, maxLength: { value: 15, message: "Too long Password." }, validate: (value) => value === newPassword ||"Values do not match."})}
                         />
                         <div
-                            className="absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer"
-                            onClick={() => setShowNewPassword(!showNewPassword)}
+                            className="absolute right-4 top-9 transform -translate-y-1/2 cursor-pointer"
+                            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
                         >
-                            {showNewPassword ? <IoEyeOff size={20} /> : <IoEye size={20} />}
+                            {showConfirmPassword ? <IoEyeOff size={20} /> : <IoEye size={20} />}
                         </div>
-                        {errors.newPassword && (
-                            <p className="text-red-500 text-sm mt-1">{errors.newPassword.message}</p>
-                        )}
+                        {console.log(errors.confirmPassword?.message)}
+                        <p className="text-red-500 text-sm h-4">{errors.newPassword?.message || errors.confirmPassword?.message}</p>
                     </div>
 
                     {/* Submit Button */}
@@ -137,7 +109,7 @@ export default function NewPassword() {
                             type="submit"
                             className="w-full bg-blue-600 hover:bg-blue-800 transition text-white py-2 px-4 rounded-lg mt-4"
                         >
-                            Update Password
+                            New Password
                         </button>
                     </div>
                 </form>
